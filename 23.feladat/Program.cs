1b17a2963cfab5fe7d09fc7461cc49140e396e45
tanulo <molnar.viktoria.1231@dszc.baross.edu.hu>
2026-09-24 10:55:04 +0200

    23.feladat

---
+++ 23.feladat/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23.feladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Add meg az első számot");
+            int szam1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Add meg a második számot");
+            int szam2 = int.Parse(Console.ReadLine());
+
+            if (szam1>szam2)
+            {
+                Console.WriteLine($"A(z) {szam1 - szam2}");
+            }
+            else if (szam2>szam1)
+            {
+                Console.WriteLine($"A(z) {szam2 - szam1} ");
+            }
+            else
+            {
+                Console.WriteLine($"A(z) {szam1} = {szam2}");
+            }
+
+
+
+            Console.ReadKey();  
+        }
+    }
+}
