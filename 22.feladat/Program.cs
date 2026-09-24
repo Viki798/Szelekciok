a5aef643c37d797bd80c92d4bfe1f87b18ed08aa
tanulo <molnar.viktoria.1231@dszc.baross.edu.hu>
2026-09-24 10:42:19 +0200

    22.feladat

---
+++ 22.feladat/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.feladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Addj meg egy számot:");
+            int szam = int.Parse(Console.ReadLine());
+            if (szam%3==0)
+            {
+                Console.WriteLine("A szám osztahtó hárommal");
+            }
+            else
+            {
+                Console.WriteLine("A szám nem osztható hárommal");
+            }
+            
+
+
+
+
+            Console.ReadKey();  
+        }
+    }
+}
